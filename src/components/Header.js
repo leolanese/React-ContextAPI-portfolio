1039f055f46b1b90436d062738868571600e6762
LeoLanese <laneseleo@gmail.com>
2019-12-18 22:44:05 +0000

    using axios to and json placeholders to mock and fetch adata

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 import Contacts from "./Contacts";
 import About from "./About";
