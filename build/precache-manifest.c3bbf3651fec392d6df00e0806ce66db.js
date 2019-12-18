fa4d31f03c47151eec91e194fa506da00ab1fe75
LeoLanese <laneseleo@gmail.com>
2019-12-18 22:42:28 +0000

    using axios to and json placeholders to mock and fetch adata

---
+++ build/precache-manifest.c3bbf3651fec392d6df00e0806ce66db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c0e1ebc1150903e088d6f6acf84cbe",
+    "url": "/leolanese/reactjs-playground/index.html"
+  },
+  {
+    "revision": "a24f84a9d2dcc9d04f2c",
+    "url": "/leolanese/reactjs-playground/static/css/2.aee84c9b.chunk.css"
+  },
+  {
+    "revision": "045ce1061644d2d2a14a",
+    "url": "/leolanese/reactjs-playground/static/css/main.37fd5e80.chunk.css"
+  },
+  {
+    "revision": "a24f84a9d2dcc9d04f2c",
+    "url": "/leolanese/reactjs-playground/static/js/2.c5303eee.chunk.js"
+  },
+  {
+    "revision": "045ce1061644d2d2a14a",
+    "url": "/leolanese/reactjs-playground/static/js/main.bb6f30a7.chunk.js"
+  },
+  {
+    "revision": "128633c499cb761a60b2",
+    "url": "/leolanese/reactjs-playground/static/js/runtime-main.99014b6e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/leolanese/reactjs-playground/static/media/logo.25bf045c.svg"
+  }
+]);
